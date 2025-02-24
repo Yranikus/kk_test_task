fb0b5921fe443c6809503062a1cc4a2a4cbe786d
Nikolai <Nikolay.Saleev@awara-it.com>
2025-02-24 15:15:35 +0300

    deleted useless import

--- src/main/java/dto/WeatherForecast.java
+++ src/main/java/dto/WeatherForecast.java
@@ -2,6 +2,4 @@ package dto;
 
 import config.constants.Source;
 
-import java.util.Date;
-
 public record WeatherForecast(String city, double temperature, double windSpeed, String weatherConditions, Source source, String date) { }
